03516dd25d6fd1916d21b5eb5127808bbbe7476a
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-12-17 05:58:15 +0600

    added => 06_class_method_params

---
+++ 06_class_method_params.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class Car
+{
+public:
+    int speed(int maxSpeed);
+};
+
+int Car::speed(int maxSpeed)
+{
+    return maxSpeed;
+}
+
+int main()
+{
+    Car myObj;
+    cout << myObj.speed(200) << endl;
+    return 0;
+}
